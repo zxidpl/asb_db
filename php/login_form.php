215673e22d283e536dec9f65eb9e652af3b413e7
zxidpl <zaidaiman02@gmail.com>
2023-03-27 13:31:02 +0800

    login and register

--- php/login_form.php
+++ php/login_form.php
@@ -1,6 +1,7 @@
 <?php
 
 @include 'config.php';
+
 // Starting the session for user
 session_start();
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
@@ -17,39 +18,54 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
    $result = $conn->query($select);
 
    if ($result->num_rows > 0) {
-     // output data of each row
+     
+      // output data of each row
      while($row = $result->fetch_assoc()) {
+      
       // Passing data from DB to server
       $_SESSION["userId"] = $row["id"];
       $_SESSION["userName"] = $row["name"];
       $_SESSION["userEmail"] = $row["email"];
+      $_SESSION["userPassword"] = $row["password"];
+
 
       header('location:user_page.php');
      }
-   } else {
+   } 
+   
+   else 
+   {
       $error = '*incorrect email or password!';
    }
 
    $conn->close();
+
 };
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <?php
 @include 'config.php';
 @include '../php/InitHTML/head.php';
 ?>
 
 <body class="background-img-login">
+
    <div class="container margin-container">
+
       <div class="row">
          <div class="col-4"></div>
          <div class="col-4">
             <div class="card">
                <div class="card-body">
+
                   <h3>login now</h3>
+
                   <form action="" method="POST">
+
                      <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">email</span>
                         <input type="email" name="email" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon1">
@@ -59,26 +75,36 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                         <span class="input-group-text" id="basic-addon2">password</span>
                         <input type="password" name="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon2">
                      </div>
+
                      <div class="input-group mb-3">
                         <button type="submit" class="btn btn-primary">log in</button>
                      </div>
+
                      <p class="error-text">
+
                         <?php
                         if (isset($error)) {
                            echo $error;
                         };
                         ?>
+
                      </p>
+
                      <p>don't have an account? <a href="register_form.php">register now</a></p>
+                 
                   </form>
+              
                </div>
             </div>
          </div>
          <div class="col-4"></div>
       </div>
    </div>
+
    <?php 
    @include '../php/InitHTML/footer.php';
    ?>
+
 </body>
+
 </html>
--- php/register_form.php
+++ php/register_form.php
@@ -4,10 +4,10 @@
 
 if(isset($_POST['submit'])){
 
-   $name = mysqli_real_escape_string($conn, $_POST['name']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $password = md5($_POST['password']);
-   $password = md5($_POST['password']);
+   $name = "name";
+   $email = "email";
+   $password = "password";
+   $password = "confirm password";
 
    $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$password' ";
 
@@ -36,56 +36,76 @@ if(isset($_POST['submit'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <?php
 @include 'config.php';
 @include '../php/InitHTML/head.php';
 ?>
+
 <body class="background-img-login">
    <div class="container margin-container">
+
       <div class="row">
          <div class="col-4"></div>
          <div class="col-4">
             <div class="card">
                <div class="card-body">
+
                   <h3>ANONYMOUS STRIKE</h3>
                   <h3>BACK</h3>
+
                   <form action="" method="POST">
+
                      <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">name</span>
                         <input id = "name" type="name" name="name" class="form-control" placeholder="name" aria-label="name" aria-describedby="basic-addon1">
                      </div>
+
                      <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon1">email</span>
                         <input id = "email" type="email" name="email" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon1">
                      </div>
+
                      <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon2">password</span>
                         <input id = "pass" type="password" name="password" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon2">
                      </div>
+
                      <div class="input-group mb-3">
                         <span class="input-group-text" id="basic-addon2">confirm password</span>
                         <input id = "password"  type="password" name="confirm password" class="form-control" placeholder="confirm password" aria-label="password" aria-describedby="basic-addon2">
                      </div>
+
                      <div class="input-group mb-3">
                         <button type="submit" class="btn btn-primary">register</button>
                      </div>
+
                      <p class="error-text">
+
                         <?php
                         if (isset($error)) {
                            echo $error;
                         };
                         ?>
+
                      </p>
+
                      <p>already have an account? <a href="login_form.php">register now</a></p>
+                  
                   </form>
+                  
                </div>
             </div>
          </div>
          <div class="col-4"></div>
       </div>
+
    </div>
+
    <?php 
    @include '../php/InitHTML/footer.php';
    ?>
+
 </body>
+
 </html>
